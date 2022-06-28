f556793b0ee91c7db4b1a8d1e4d36c552de99c7a
Anton <toxajlb@yandex.ru>
2022-06-28 12:38:20 +0300

    commit

--- dist/js/script.js
+++ dist/js/script.js
@@ -4,9 +4,27 @@ $(document).ready(function(){
         adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
+        fade: true,
+        cssEasy: 'linear',
+        //autoplay: true,
+        //autoplaySpeed: 1200,
         responsive: [
             {
                 breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            },
+            {
+                breakpoint: 540,
                 settings: {
                     dots: true,
                     arrows: false
