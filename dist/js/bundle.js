5c541319ee7bd7231bc6a9465ffb6da21f0dcd39
Anton <toxajlb@yandex.ru>
2022-07-16 15:07:51 +0300

    commit

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -132,57 +132,39 @@ function menu() {
   const hamburger = document.querySelector('.hamburger'),
         menu = document.querySelector('.menu'),
         menuSecond = document.querySelector('.menu-second'),
+        menuPrice = document.querySelector('.menu-price'),
         closeElem = document.querySelector('.menu__close'),
         closeElemSecond = document.querySelector('.menu-second__close'),
-        sublistFirst = document.querySelector('#sublist-first'),
-        sublistSecond = document.querySelector('#sublist-second'),
-        sublistThird = document.querySelector('#sublist-third'),
-        sublistFourth = document.querySelector('#sublist-fourth'),
-        sublistFifth = document.querySelector('#sublist-fifth'),
+        closeElemPrice = document.querySelector('.menu-price__close'),
         asphalting = document.querySelector('#asphalting'),
-        linkFirst = document.querySelector('#link-first'),
-        linkSecond = document.querySelector('#link-second'),
-        linkThird = document.querySelector('#link-third'),
-        linkFourth = document.querySelector('#link-fourth'),
-        linkFifth = document.querySelector('#link-fifth'),
-        closeFirst = document.querySelector('#close-first'),
-        closeSecond = document.querySelector('#close-second'),
-        closeThird = document.querySelector('#close-third'),
-        closeFourth = document.querySelector('#close-fourth'),
-        closeFifth = document.querySelector('#close-fifth');
-  hamburger.addEventListener('click', () => {
-    menu.classList.add('active');
-  });
-  closeElem.addEventListener('click', () => {
-    menu.classList.remove('active');
-  });
-  asphalting.addEventListener('mouseover', () => {
-    menuSecond.classList.add('active');
-  });
-  closeElemSecond.addEventListener('click', () => {
-    menuSecond.classList.remove('active');
-  });
+        prices = document.querySelector('#prices');
 
-  function menuCall(item, secondItem, thirdItem) {
-    item.addEventListener('mousedown', () => {
-      secondItem.style.display = 'block';
+  function menuChoise(elem1, elem2, elem3) {
+    elem1.addEventListener('click', () => {
+      elem2.classList.add('active');
     });
-    secondItem.addEventListener('mouseover', e => {
-      e.currentTarget.style.display = 'block';
-    });
-    secondItem.addEventListener('mouseout', e => {
-      e.currentTarget.style.display = 'none';
-    });
-    thirdItem.addEventListener('click', () => {
-      secondItem.style.display = 'none';
+    elem3.addEventListener('click', () => {
+      elem2.classList.remove('active');
     });
   }
 
-  menuCall(linkFirst, sublistFirst, closeFirst);
-  menuCall(linkSecond, sublistSecond, closeSecond);
-  menuCall(linkThird, sublistThird, closeThird);
-  menuCall(linkFourth, sublistFourth, closeFourth);
-  menuCall(linkFifth, sublistFifth, closeFifth);
+  menuChoise(hamburger, menu, closeElem);
+  menuChoise(asphalting, menuSecond, closeElemSecond);
+  menuChoise(prices, menuPrice, closeElemPrice);
+  $('.menu-second__pointer').each(function (i) {
+    $(this).on('click', function (e) {
+      e.preventDefault();
+      $('.menu-second__sublist').eq(i).toggleClass('.menu-second__sublist active');
+      $('.menu-second__pointer').eq(i).toggleClass('.menu-second__pointer active');
+    });
+  });
+  $('.menu-price__pointer').each(function (i) {
+    $(this).on('click', function (e) {
+      e.preventDefault();
+      $('.menu-price__sublist').eq(i).toggleClass('.menu-price__sublist active');
+      $('.menu-price__pointer').eq(i).toggleClass('.menu-price__pointer active');
+    });
+  });
 }
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (menu);
